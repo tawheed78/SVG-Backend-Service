74b930e8341b8b71ecf41078963b77a8ab0ebafa
tawheed78 <119650487+tawheed78@users.noreply.github.com>
2024-05-02 01:33:23 +0530

    Update settings.py

--- svg_backend/svg_backend/settings.py
+++ svg_backend/svg_backend/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['svg-backend-service-1.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
