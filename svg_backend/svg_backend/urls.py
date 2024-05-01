1190f67eef66c3d641ba7fedc98e0a0abb0be634
tawheed78 <tawheed.chilwan55@gmail.com>
2024-05-02 03:05:52 +0530

    update readme

--- svg_backend/svg_backend/urls.py
+++ svg_backend/svg_backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/apis/', include('games.urls')),
+    path('v1/', include('games.urls')),
 ]
 
 # REST_FRAMEWORK = {(
